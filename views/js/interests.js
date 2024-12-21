ce8c555ab4451004e5709081a085c305ef586f27
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-12-21 20:57:15 +0100

    Testing again

--- views/js/interests.js
+++ views/js/interests.js
@@ -135,8 +135,7 @@ window.addEventListener('load', async function () {
 
         // Function to add user interest by making a request
         async function addUserInterest(userId, interestId) {
-            const response = await fetch(`https://toriando19.github.io/database/postgres/api-postgres.mjs?user_interest_user=${userId}&user_interest_interest=${interestId}`, {
-            // const response = await fetch(`http://localhost:3000/add-userinterest?user_interest_user=${userId}&user_interest_interest=${interestId}`, {
+            const response = await fetch(`${interestUrl}?user_interest_user=${userId}&user_interest_interest=${interestId}`, {
                 method: 'GET'
             });
             if (!response.ok) {
