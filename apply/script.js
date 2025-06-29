ef626b6d4c318798f056e39803d46d729e670dee
toriando19 <viktoria_andonovska@hotmail.com>
2025-06-29 12:45:17 +0200

    Tato <3

--- apply/script.js
+++ apply/script.js
@@ -1,1 +1,17 @@
-// alert("Hello");
+const apply = document.querySelector('.apply');
+const universeLinks = document.querySelectorAll('.apply-universe a');
+
+universeLinks.forEach(link => {
+    const bgImage = link.getAttribute('data-bg');
+
+    link.addEventListener('mouseover', () => {
+        apply.style.backgroundImage = `url(${bgImage})`;
+    });
+
+    link.addEventListener('mouseout', () => {
+        apply.style.backgroundImage = ''; // Reset to default
+    });
+});
+
+
+
